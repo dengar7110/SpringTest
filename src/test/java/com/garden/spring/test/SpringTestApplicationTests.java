419dc75477809a2c825d86f36b3d776679f85b08
garden <lmnoptnt@gmail.com>
2024-08-06 19:44:22 +0900

    [홍정원] - SpringTest 프로젝트 최초 생성

---
+++ src/test/java/com/garden/spring/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garden.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
